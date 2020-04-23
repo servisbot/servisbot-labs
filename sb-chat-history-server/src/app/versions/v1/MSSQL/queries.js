efc0f1e8b374c5c569bb119817777a5693435560
Steve <stephen.walsh@servisbot.com>
2020-04-23 13:23:46 +0100

    Fix for db_name

--- sb-chat-history-server/src/app/versions/v1/MSSQL/queries.js
+++ sb-chat-history-server/src/app/versions/v1/MSSQL/queries.js
@@ -1,16 +1,15 @@
 //  Database queries
 
-const { DB_NAME } = process.env;
 const queries = {
-  insertMessagesRow: () => `INSERT INTO [${DB_NAME}].[dbo].[Messages] 
+  insertMessagesRow: () => `INSERT INTO [ServisbotChatHistory].[dbo].[Messages] 
     (ConversationId, Organization, [Identity], MessageId, Contents, Timestamp, SbId) 
     VALUES (@conversationId, @organization, @identity, @messageId, @contents, @timestamp, @sbid);`,
 
-  insertInteractionsRow: () => `INSERT INTO [${DB_NAME}].[dbo].[Interactions] 
+  insertInteractionsRow: () => `INSERT INTO [ServisbotChatHistory].[dbo].[Interactions] 
     (ConversationId, Organization, [Identity], MessageId, Contents, Timestamp, SbId) 
     VALUES (@conversationId, @organization, @identity, @messageId, @contents, @timestamp, @sbid);`,
 
-  getRow: () => `SELECT id FROM [${DB_NAME}].[dbo].[Messages] 
+  getRow: () => `SELECT id FROM [ServisbotChatHistory].[dbo].[Messages] 
     WHERE ConversationId = @conversationId 
     AND Organization = @organization 
     AND [Identity] = @identity;`,
@@ -18,15 +17,15 @@ const queries = {
   getRows: () => `SELECT m.id AS id, 
     m.Contents AS messageContents, 
     i.Contents AS interactionContents 
-    FROM [${DB_NAME}].[dbo].[Messages] AS m 
-    LEFT JOIN [${DB_NAME}].[dbo].[Interactions] AS i 
+    FROM [ServisbotChatHistory].[dbo].[Messages] AS m 
+    LEFT JOIN [ServisbotChatHistory].[dbo].[Interactions] AS i 
     ON m.MessageId = i.MessageId 
     WHERE m.ConversationId = @conversationId 
     AND m.Organization = @organization 
     AND m.[Identity] = @identity 
     ORDER BY m.id DESC OFFSET 0 ROWS FETCH NEXT 50 ROWS ONLY;`,
 
-  getRowsWithNext: () => `SELECT Contents FROM [${DB_NAME}].[dbo].[Messages] 
+  getRowsWithNext: () => `SELECT Contents FROM [ServisbotChatHistory].[dbo].[Messages] 
     WHERE ConversationId = @conversationId 
     AND Organization = @organization 
     AND [Identity] = @identity  
