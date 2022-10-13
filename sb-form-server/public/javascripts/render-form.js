ffbe22622679260fb9f86a505877f5f03e0014e2
chris-doyle <chris@servisbot.com>
2022-10-13 09:58:21 +0100

    unknown impact - possible test changes

--- sb-form-server/public/javascripts/render-form.js
+++ sb-form-server/public/javascripts/render-form.js
@@ -2,6 +2,7 @@
 
 window.onload = () => {
   Formio.createForm(document.getElementById('formio'), `${window.location.origin}/templates/${formName}`, {}).then((form) => {
+    form.url=`${window.location.origin}/templates/${formName}`
     form.on('Cancel', () => {
       window.parent.parent.postMessage('closeServisBotTask', '*');
     });
